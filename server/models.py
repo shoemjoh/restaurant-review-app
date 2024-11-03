d379199bc626daa94bab926abdea4d0b914bfee9
John Shoemaker <shoemjoh@gmail.com>
2024-11-03 13:50:59 -0500

    initial User table

--- server/models.py
+++ server/models.py
@@ -1,6 +1,15 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
